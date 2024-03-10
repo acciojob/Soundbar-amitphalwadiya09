700717fe40f1434cef6ba50c5698f3a29ea598dc
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-10 16:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let mySound = new Audio('https://www.youtube.com/watch?v=0mfJn604GT4');
+let sound1=new Audio();
+sound1.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
 
-// Get the button elements and add click event listeners
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.btn, .stop').forEach(function (button) {
-        button.addEventListener('click', function (eventDetails) {
-            eventDetails.preventDefault();
-            mySound.play();
-        });
-    });
-});
+let sound2=new Audio();
+sound2.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+
+let sound3=new Audio();
+sound3.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+
+let sound4=new Audio();
+sound4.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+
+let sound5=new Audio();
+sound5.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+
+let sound6=new Audio();
+sound6.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+
