0275fe6f0bde7ec715807beeaf3c4b81536485b3
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-10 14:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let mySound = new Audio('https://www.youtube.com/watch?v=0mfJn604GT4');
+
+// Get the button elements and add click event listeners
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.btn, .stop').forEach(function (button) {
+        button.addEventListener('click', function (eventDetails) {
+            eventDetails.preventDefault();
+            mySound.play();
+        });
+    });
+});
