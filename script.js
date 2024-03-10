a0926de5d76ba29f901d93cc2ee4aff01243d3e7
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-10 16:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let sound1=new Audio();
-sound1.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+let sound1 = new Audio();
+sound1.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/applause.mp3';
 
-let sound2=new Audio();
-sound2.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+let sound2 = new Audio();
+sound2.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/boo.mp3';
 
-let sound3=new Audio();
-sound3.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+let sound3 = new Audio();
+sound3.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/gasp.mp3';
 
-let sound4=new Audio();
-sound4.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+let sound4 = new Audio();
+sound4.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/tada.mp3';
 
-let sound5=new Audio();
-sound5.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
+let sound5 = new Audio();
+sound5.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/victory.mp3';
 
-let sound6=new Audio();
-sound6.src='C:\Users\amitp\OneDrive\Desktop\Javascript\soundbar\applause.mp3'
-
+let sound6 = new Audio();
+sound6.src = 'C:/Users/amitp/OneDrive/Desktop/Javascript/soundbar/wrong.mp3';
